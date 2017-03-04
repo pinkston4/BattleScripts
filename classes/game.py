933d170a05063d50dc483f14fa29a83cb4678eea
john pinkston <pinkstonjack2@gmail.com>
2017-03-04 16:26:44 -0600

    refactored for pep

--- classes/game.py
+++ classes/game.py
@@ -1,149 +1,136 @@
 import random
 
+
 class Bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
-class Person():
-	def __init__(self, hp, mp, atk, df, magic, items, name = 'Cartman'):
-		self.maxhp = hp
-		self.hp = hp
-		self.maxmp = mp
-		self.mp = mp
-		self.atkl = atk - 10
-		self.atkh = atk + 10
-		self.df = df
-		self.magic = magic
-		self.items = items
-		self.name = name
-		self.actions = ['Melee', 'Magic', 'Items']
+class Person:
+    def __init__(self, hp, mp, atk, df, magic, items, name='Cartman'):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.items = items
+        self.name = name
+        self.actions = ['Melee', 'Magic', 'Items']
 
-	def generate_damage(self):
-		return random.randrange(self.atkl, self.atkh)
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
 
-	def take_damage(self, damage):
-		self.hp -= damage
-		if self.hp < 0:
-			self.hp = 0
-		return self.hp
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
 
-	def get_hp(self):
-		return self.hp
+    def get_hp(self):
+        return self.hp
 
-	def get_max_hp(self):
-		return self.maxhp
+    def get_max_hp(self):
+        return self.maxhp
 
-	def get_mp(self):
-		return self.mp
+    def get_mp(self):
+        return self.mp
 
-	def get_max_mp(self):
-		return self.maxmp
+    def get_max_mp(self):
+        return self.maxmp
 
-	def get_name(self):
-		return self.name
+    def get_name(self):
+        return self.name
 
-	def reduce_mp(self, cost):
-		self.mp -= cost
+    def reduce_mp(self, cost):
+        self.mp -= cost
 
-	def reduce_item_quantity(self, item):
-		self.items[item].quantity -=1
+    def reduce_item_quantity(self, item):
+        self.items[item].quantity -= 1
 
-	def choose_target(self, enemies):
-		i = 1
-		print('\n' + Bcolors.FAIL + Bcolors.BOLD + 'TARGET: ' + Bcolors.ENDC)
-		for enemy in enemies:
-			print('    ', str(i), '.', enemy.name)
-			i += 1
+    def choose_target(self, enemies):
+        i = 1
+        print('\n' + Bcolors.FAIL + Bcolors.BOLD + 'TARGET: ' + Bcolors.ENDC)
+        for enemy in enemies:
+            print('    ', str(i), '.', enemy.name)
+            i += 1
 
-	def choose_action(self):
-		i = 1
-		print('\n' + Bcolors.OKGREEN + Bcolors.BOLD + 'ACTIONS: ' + Bcolors.ENDC)
-		for item in self.actions:
-			print('    ', str(i) + '.', item)
-			i += 1
+    def choose_action(self):
+        i = 1
+        print('\n' + Bcolors.OKGREEN + Bcolors.BOLD + 'ACTIONS: ' + Bcolors.ENDC)
+        for item in self.actions:
+            print('    ', str(i) + '.', item)
+            i += 1
 
-	def choose_magic(self):
-		i = 1
-		print('\n' + Bcolors.OKGREEN + Bcolors.BOLD + 'MAGIC: ' + Bcolors.ENDC)
-		for spell in self.magic:
-			print('    ', str(i) + '.', spell.name, '(cost:', str(spell.cost) + ')')
-			i += 1
+    def choose_magic(self):
+        i = 1
+        print('\n' + Bcolors.OKGREEN + Bcolors.BOLD + 'MAGIC: ' + Bcolors.ENDC)
+        for spell in self.magic:
+            print('    ', str(i) + '.', spell.name, '(cost:', str(spell.cost) + ')')
+            i += 1
 
-	def heal(self, dmg):
-		self.hp += dmg
-		if self.hp > self.maxhp:
-			self.hp = self.maxhp
+    def heal(self, dmg):
+        self.hp += dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
 
-	def choose_item(self):
-		i = 1
-		print('\n' + Bcolors.OKGREEN + Bcolors.BOLD + 'ITEMS: ' + Bcolors.ENDC)
-		for item in self.items:
-			print('    ', str(i) + '. ' + item.name + ' : ' + item.description + ' (x' + str(item.quantity) + ')')
-			i += 1
+    def choose_item(self):
+        i = 1
+        print('\n' + Bcolors.OKGREEN + Bcolors.BOLD + 'ITEMS: ' + Bcolors.ENDC)
+        for item in self.items:
+            print('    ', str(i) + '. ' + item.name + ' : ' + item.description + ' (x' + str(item.quantity) + ')')
+            i += 1
 
-	def get_score_board(self):
-		hp_bar = ''
-		bar_ticks = (self.hp / self.maxhp) * 100 / 4
-		mp_bar = ''
-		mp_ticks = (self.mp / self.maxmp) * 100 / 10
+    def get_score_board(self):
+        hp_bar = ''
+        bar_ticks = (self.hp / self.maxhp) * 100 / 4
+        mp_bar = ''
+        mp_ticks = (self.mp / self.maxmp) * 100 / 10
 
-		while bar_ticks > 0:
-			hp_bar += '█'
-			bar_ticks -= 1
-		while len(hp_bar) < 25:
-			hp_bar += ' '
-		while mp_ticks > 0:
-			mp_bar += '█'
-			mp_ticks -= 1
-		while len(mp_bar) < 10:
-			mp_bar += ' '
+        while bar_ticks > 0:
+            hp_bar += '█'
+            bar_ticks -= 1
+        while len(hp_bar) < 25:
+            hp_bar += ' '
+        while mp_ticks > 0:
+            mp_bar += '█'
+            mp_ticks -= 1
+        while len(mp_bar) < 10:
+            mp_bar += ' '
 
+        print('\n')
+        print('NAME                    HP                                      MP')
+        print(Bcolors.BOLD + self.name + '       ' + str(self.hp) + '/' + str(
+            self.maxhp) + '|' + Bcolors.OKGREEN + hp_bar + Bcolors.ENDC + Bcolors.BOLD + '|     ' + str(
+            self.mp) + '/' + str(self.maxmp) + ' |' + Bcolors.OKBLUE + mp_bar + Bcolors.ENDC + '|')
 
-		print('\n')
-		print('NAME                    HP                                      MP')
-		print(Bcolors.BOLD + self.name + 		'       '+ str(self.hp) + '/' +str(self.maxhp) +'|' + Bcolors.OKGREEN + hp_bar + Bcolors.ENDC + Bcolors.BOLD +'|     '+ str(self.mp) + '/' + str(self.maxmp) +' |'+ Bcolors.OKBLUE + mp_bar + Bcolors.ENDC +'|')
+    def get_enemy_score_board(self):
+        hp_bar = ''
+        bar_ticks = (self.hp / self.maxhp) * 100 / 4
+        mp_bar = ''
+        mp_ticks = (self.mp / self.maxmp) * 100 / 10
 
-	def get_enemy_score_board(self):
-		hp_bar = ''
-		bar_ticks = (self.hp / self.maxhp) * 100 / 4
-		mp_bar = ''
-		mp_ticks = (self.mp / self.maxmp) * 100 / 10
+        while bar_ticks > 0:
+            hp_bar += '█'
+            bar_ticks -= 1
+        while len(hp_bar) < 25:
+            hp_bar += ' '
+        while mp_ticks > 0:
+            mp_bar += '█'
+            mp_ticks -= 1
+        while len(mp_bar) < 10:
+            mp_bar += ' '
 
-		while bar_ticks > 0:
-			hp_bar += '█'
-			bar_ticks -= 1
-		while len(hp_bar) < 25:
-			hp_bar += ' '
-		while mp_ticks > 0:
-			mp_bar += '█'
-			mp_ticks -= 1
-		while len(mp_bar) < 10:
-			mp_bar += ' '
-
-
-		print('\n')
-		print('NAME                    HP                                      MP')
-		print(Bcolors.BOLD + self.name + 		'       '+ str(self.hp) + '/' +str(self.maxhp) +'|' + Bcolors.FAIL + hp_bar + Bcolors.ENDC + Bcolors.BOLD +'|     '+ str(self.mp) + '/' + str(self.maxmp) +' |'+ Bcolors.OKBLUE + mp_bar + Bcolors.ENDC +'|')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print('\n')
+        print('NAME                    HP                                      MP')
+        print(Bcolors.BOLD + self.name + '       ' + str(self.hp) + '/' + str(
+            self.maxhp) + '|' + Bcolors.FAIL + hp_bar + Bcolors.ENDC + Bcolors.BOLD + '|     ' + str(
+            self.mp) + '/' + str(self.maxmp) + ' |' + Bcolors.OKBLUE + mp_bar + Bcolors.ENDC + '|')
--- classes/magic.py
+++ classes/magic.py
@@ -1,14 +1,15 @@
 import random
 
+
 class Spell:
 
-	def __init__(self, name, cost, dmg, thisType):
-		self.name = name
-		self.cost = cost
-		self.dmg = dmg
-		self.thisType = thisType
+    def __init__(self, name, cost, dmg, this_type):
+        self.name = name
+        self.cost = cost
+        self.dmg = dmg
+        self.thisType = this_type
 
-	def generate_damage(self):
-		low = self.dmg - 15
-		high = self.dmg + 15
-		return random.randrange(low, high)
+    def generate_damage(self):
+        low = self.dmg - 15
+        high = self.dmg + 15
+        return random.randrange(low, high)
